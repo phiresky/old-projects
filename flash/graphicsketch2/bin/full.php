dbc3f6f64aba1d756f32f1be81a54f42e00135aa
phiresky <phireskyde+git@gmail.com>
2019-02-21 21:53:35 +0100

    flash

---
+++ flash/graphicsketch2/bin/full.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Digital Spirograph</title>
+</head>
+<body bgcolor="#ffffff" style="margin:0; padding:0">
+<object width="100%" height="98%">
+<param name="movie" value="graphicsketch2.swf?i=<?php echo $_GET["i"] . "&s=" . $_GET["s"]; ?> ">
+<embed src="graphicsketch2.swf?i=<?php echo $_GET["i"] . "&s=" . $_GET["s"]; ?>" width=100% height=100%>
+</embed>
+</object>
---
+++ flash/graphicsketch2/bin/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>hi</title>
+</head>
+<body bgcolor="#ffffff">
+<object width="800" height="600">
+<param name="movie" value="graphicsketch2.swf?i=<?php echo $_GET["i"] . "&s=" . $_GET["s"]; ?> ">
+<embed src="graphicsketch2.swf?i=<?php echo $_GET["i"] . "&s=" . $_GET["s"]; ?>" width="800" height="600">
+</embed>
+</object>
